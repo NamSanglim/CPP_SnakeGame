004f7164c50ee193f0201588612a3a107e077e18
NamSanglim <54923245+NamSanglim@users.noreply.github.com>
2020-06-09 01:55:54 +0900

    Update MakeSnakeGame.cpp

--- MakeSnakeGame.cpp
+++ MakeSnakeGame.cpp
@@ -22,80 +22,82 @@ public:
 int main() {
   srand(time(NULL));
   initscr();
-
-  start_color();
-  //팔레트 설정
-  init_pair(1, COLOR_BLACK, COLOR_CYAN);
-  init_pair(2, COLOR_YELLOW, COLOR_GREEN);
-
-  bkgd(COLOR_PAIR(1));
-  attron(COLOR_PAIR(2));
-  border('*','*','*','*','*','*','*','*');
-  attroff(COLOR_PAIR(2));
-
   noecho();
   curs_set(0);    //화면에 보이는 커서 설정, 0 : 커서 안보이게
   keypad(stdscr, true);
-  timeout(200);    //수가 클 수록 느리게 작을수록 빠르게 이동, 틱
+  timeout(200);
 
   list<Snake> snakes;
   list<Snake>::iterator it;
 
   bool quit = false;
-  int point = 0;
-  int dir = 2;
+  int point = 0; //게임이 끝났을 때 최종 점수.
+  int dir = 2; // 처음에는 무조건 오른쪽으로 간다.
+  int end_dir = 0;
+  int x_dir = 0;
   int foodX = rand()%29 +1;
-  int foodY = rand()%10 +1;
-  int ch;
+  int foodY = rand()%10 +1; //최종도착지 좌표. 랜덤으로 정해진다.
+  int ch; //방향키 입력 받기
 
-  for(int i = 0; i<5;i++)
-    snakes.push_front(Snake(3+i, 3));
+  for(int i = 0; i < 3; i++)
+    snakes.push_front(Snake(3+i, 3)); // 처음 snake의 좌표. 3개
 
   while(!quit){
+    x_dir = dir;
     ch = getch();
     switch (ch) {
       case KEY_LEFT:
-        dir = '1';
+        if(dir!= 2 ) dir = 1;
+        else end_dir = 1;
         break;
       case KEY_RIGHT:
-        dir = '2';
+        if(dir != 1) dir = 2;
+        else end_dir = 1;
         break;
       case KEY_UP:
-        dir = '4';
+        if(dir != 4) dir = 3;
+        else end_dir = 1;
         break;
       case KEY_DOWN:
-        dir = '3';
+        if(dir!= 3) dir = 4;
+        else end_dir = 1;
         break;
-      case KEY_BACKSPACE:
-         quit = true;
+      case 'q':
+        quit = true;
         break;
+    } // dir값 지정하기 위한 switch-case문. 방향키를 입력받아 적용시키기 위함이다.
+
+    switch(end_dir){
+      case 1:
+      quit = true;
+      break;
     }
+    int x = 0, y = 0;
     Snake logic = snakes.front();
-    int x = logic.getX();
-    int y = logic.getY();
+    x += logic.getX();
+    y += logic.getY(); //head 부분 좌표
 
-    if(dir == 1) x--;       //move to left
-    else if(dir == 2) x++;  //move to right
-    else if(dir == 3) y--;  //move to down
-    else if(dir == 4) y++;  //move to up
+    if(dir == 1) --x;       //move to left
+    else if(dir == 2) ++x;  //move to right
+    else if(dir == 3) --y;  //move to down
+    else if(dir == 4) ++y;  //move to up
 
-    snakes.push_front(Snake(x, y));
+    snakes.push_front(Snake(x, y)); // head 바꿔주기
 
-    if(x == foodX && y==foodY){
-      foodX= rand()%29;
-      foodY =rand()%10;
-      point++;
+    if(x == foodX && y == foodY){
+      foodX = rand() % 29;
+      foodY = rand() % 10;
+      point++; // 목표 맞췄다면 다시 목표 설정하고 point 1점씩 증가.
     }
     else
-      snakes.pop_back();
+      snakes.pop_back(); // 좌표다 못맞추었을 때 tail자름.
 
-    if(y > 50 || x > 50 || y < -50 || x < -50) //충족되지 않으면 quit = true 되어 게임 끝, 다시 말해 벽에 닿으면 게임 끝
-      quit = true;
-    erase();
-    mvaddch(foodY, foodX, 'X');
+    //if(y > 11 || x > 29 || y < 0 || x < -1) quit = true;
+    erase(); // snake의 개수 유지하고 출력해주도록 지워주기
+    mvaddch(foodY, foodX, 'X'); // 목표지점 출력
     for(it = snakes.begin(); it != snakes.end();it++){
-      mvaddch((*it).getY(), (*it).getX(), 'o');
-      if((*it).getY()==y && (*it).getX()==x && it!=snakes.begin()) quit= true;
+      mvaddch((*it).getY(), (*it).getX(), 'o'); // snake 출력
+      //if((*it).getY()==y && (*it).getX()==x && it!=snakes.begin()) quit= true;
     }
     mvprintw(0, 0, "You got %i points. 'q' to quit.\n", point);
     refresh();
